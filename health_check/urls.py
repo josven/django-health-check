bac2f2cb586ed6864ee324bb2cbc6eefb7ddca1c
josven <jonas.s.svensson@gmail.com>
2016-01-05 15:39:46 +0100

    Remove RemovedInDjango110Warning

--- health_check/urls.py
+++ health_check/urls.py
@@ -1,8 +1,12 @@
+from __future__ import absolute_import, unicode_literals
+
 from django.conf.urls import patterns, include, url
 
-import health_check
-health_check.autodiscover()
+from . import autodiscover
+from .views import home
 
-urlpatterns = patterns('',
-    url(r'^$', 'health_check.views.home', name='health_check_home'),
-)
+autodiscover()
+
+urlpatterns = [
+    url(r'^$', home, name='health_check_home'),
+]
